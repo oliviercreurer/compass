077054764bc466676f2ce8035e6355988a9de31e
Olivier Creurer <39808162+oliviercreurer@users.noreply.github.com>
2020-03-04 13:13:03 -0500

    update v2.1
    adds decimator ugen
    create fixed loop window
    record toggle command

--- compass.lua
+++ compass.lua
@@ -1,5 +1,5 @@
 --
--- Compass (2.0.1)
+-- Compass (2.1)
 -- Command-based looper
 -- @olivier
 -- llllllll.co/t/compass/25192
@@ -17,6 +17,8 @@
 -- K1(hold) + E2 : set loop start
 -- K1(hold) + E3 : set loop end
 
+engine.name = "Decimatec"
+
 local rate = 1
 local ratePos = 5
 local rec = 0
@@ -27,7 +29,9 @@ local edit = 1
 local clkSpd = 1
 
 local loopStart = 1
+local sPoint = 1
 local loopEnd = 65
+local ePoint = 65
 local loopLength = 64
 
 local fade = 0.05
@@ -41,7 +45,7 @@ local down_time = 0
 local KEYDOWN1 = 0
 local KEYDOWN2 = 0
 
-local pages = {"EDIT", "COMMANDS/SEQUENCE", "COMMANDS/SEQUENCE", "COMMANDS/SOFTCUT", "COMMANDS/SOFTCUT", "COMMANDS/SOFTCUT + ^^"}
+local pages = {"EDIT", "COMMANDS/SEQUENCE", "COMMANDS/SEQUENCE", "COMMANDS/SOFTCUT", "COMMANDS/SOFTCUT", "COMMANDS/SOFTCUT", "COMMANDS/CROW"}
 local positions = {0,0}
 local rates = {-2,-1,-0.5,0.5,1,2}
 
@@ -128,17 +132,14 @@ end
 
 function scLevel(x)
   x = util.clamp(x,0,5)
-  -- print("input stream (level):"..(x*2)/10)
   audio.level_cut((x*2)/10)
 end
 
 function scRate(x)
   x = util.clamp(x,-4,4)
-  -- print("input stream (rate):"..x)
   for i=1,2 do
     softcut.rate(i,x)
   end
-  -- audio.level_cut(1)
 end
   
 
@@ -168,22 +169,23 @@ function rateInc() for i=1,2 do softcut.rate(i,rates[math.random(4,6)]) end end
 function rateDec() for i=1,2 do softcut.rate(i,rates[math.random(1,3)]) end end
 function rateRnd() for i=1,2 do softcut.rate(i,rates[math.random(1,6)]) end end
 function sPosStart() for i=1,2 do softcut.position(i,loopStart) end end
-function sPosRnd() for i=1,2 do softcut.position(i,1+math.random(loopStart,loopEnd)) end end
-function loopRnd() loopStart = math.random(1,loopEnd-1) ; loopEnd = math.random(loopStart+1,loopLength) end
+function sPosRnd() for i=1,2 do softcut.position(i,math.random(loopStart,loopEnd)) end end
+function loopRnd() loopStart = math.random(sPoint,loopEnd-1) ; loopEnd = math.random(loopStart+1,ePoint) end
 function rndPanL() panL = math.random(0,9)/-10 end
 function rndPanR() panR = math.random(0,9)/10 end
+function toggleRec() rec = 1 - rec end
 
 -- Crow
 function crowTrig() crow.output[1].execute() end
 function crowRnd() crow.output[2].volts = math.random(10) end
 
-local act = {metroSteady,metroDec,metroInc,metroBottom,metroTop,stepRnd,rateForward,rateReverse,rateInc,rateDec,rateRnd,sPosStart,sPosRnd,loopRnd,rndPanL,rndPanR,crowTrig,crowRnd}
-local actCrow = {stepRnd,rateForward,rateReverse,rateInc,rateDec,rateRnd,sPosStart,sPosRnd,loopRnd,rndPanL,rndPanR,crowTrig,crowRnd}
-local COMMANDS = 18
-local COMMANDScrow = 13
-local label = {"C", "<", ">", "[", "]", "?", "F", "R", "+", "-", "!", "1", "P", "L", "(", ")", "T", "V"}
-local labelCrow = {"?", "F", "R", "+", "-", "!", "1", "P", "L", "(", ")", "T", "V" }
-local description = {"- Steady clock", "- / clock speed by 2", "- * clock speed by 2", "- Bottom speed", "- Top speed", "- Jump to random step", "- Forward rate (1x)", "- Reverse rate (1x)", "- Increase rate", "- Decrease rate", "- Random rate", "- Loop start", "- Random position", "- Random loop start/end", "- Random pan pos (L)", "- Random pan pos (R)", "- Pulse (crow out 1)", "- Rnd voltage (crow out 2)"}
+local act = {metroSteady,metroDec,metroInc,metroBottom,metroTop,stepRnd,rateForward,rateReverse,rateInc,rateDec,rateRnd,sPosStart,sPosRnd,loopRnd,rndPanL,rndPanR,toggleRec,crowTrig,crowRnd}
+local actCrow = {stepRnd,rateForward,rateReverse,rateInc,rateDec,rateRnd,sPosStart,sPosRnd,loopRnd,rndPanL,rndPanR,toggleRec,crowTrig,crowRnd}
+local COMMANDS = 19
+local COMMANDScrow = 14
+local label = {"C", "<", ">", "[", "]", "?", "F", "R", "+", "-", "!", "1", "P", "L", "(", ")", "::", "T", "V"}
+local labelCrow = {"?", "F", "R", "+", "-", "!", "1", "P", "L", "(", ")", "::", "T", "V" }
+local description = {"- Steady clock", "- / clock speed by 2", "- * clock speed by 2", "- Bottom speed", "- Top speed", "- Jump to random step", "- Forward rate (1x)", "- Reverse rate (1x)", "- Increase rate", "- Decrease rate", "- Random rate", "- Loop start", "- Random position", "- Random loop start/end", "- Random pan pos (L)", "- Random pan pos (R)", "- Toggle record", "- Pulse (crow out 1)", "- Rnd voltage (crow out 2)"}
 
 function init()
   
@@ -220,13 +222,19 @@ function init()
   params:set_action("PAN_L", function(x) panL = x end)
   params:add_control("PAN_SLEW", "Pan (slew)", controlspec.new(0,2,'lin', 0.01,0.25))
   params:set_action("PAN_SLEW", function(x) for i=1,2 do softcut.pan_slew_time(i,x) end end)
-  params:add_control("LOOP START","Loop Start",controlspec.new(1,loopEnd-1,'lin',1,1))
-  params:set_action("LOOP START", function(x) loopStart = util.clamp(x,1,loopEnd-1) end)
-  params:add_control("LOOP END","Loop End",controlspec.new(loopStart+1,65,'lin',1,65))
-  params:set_action("LOOP END", function(x) loopEnd = util.clamp(x,loopStart+1,65) end)
+  params:add_control("START POINT","Start Point",controlspec.new(1,ePoint-1,'lin',1,1))
+  params:set_action("START POINT", function(x) sPoint = util.clamp(x,1,ePoint-1); loopStart = sPoint end)
+  params:add_control("END POINT","End Point",controlspec.new(sPoint+1,65,'lin',1,65))
+  params:set_action("END POINT", function(x) ePoint = util.clamp(x,sPoint+1,65); loopEnd = ePoint end)
+  -- params:add_control("LOOP START","Loop Start",controlspec.new(sPoint,ePoint,'lin',1,1))
+  -- params:set_action("LOOP START", function(x) loopStart = util.clamp(x,sPoint,ePoint-1) end)
+  -- params:add_control("LOOP END","Loop End",controlspec.new(sPoint,ePoint,'lin',1,65))
+  -- params:set_action("LOOP END", function(x) loopEnd = util.clamp(x,sPoint+1,ePoint) end)
   
   params:add_separator()
   
+  params:add_control("bit_depth", "bit depth", controlspec.new(4, 31, "lin", 0, 31, ''))
+  params:set_action("bit_depth", function(x) engine.sdepth(x) end)
   params:add_control("CUT_LEVEL", "Cut level", controlspec.new(0,1,'lin',0.01,1))
   params:set_action("CUT_LEVEL", function(x) audio.level_cut(x) end)
   params:add_control("IN_LEVEL", "Input level", controlspec.new(0,1,'lin',0.01,1))
@@ -311,7 +319,6 @@ function count()
     actCrow[step[pos]]()
   end
   redraw()
-  --print("ratePos " .. ratePos)
 end
 
 function cutReset()
@@ -336,27 +343,23 @@ function enc(n,d)
   if n==1 then
     if KEYDOWN1 == 1 then
       STEPS = util.clamp(STEPS+d,2,16)
-      print("Steps " .. STEPS)
     else 
       pageNum = util.clamp(pageNum+d,1,#pages)
     end
   elseif n==2 then
     if KEYDOWN1 == 1 then
-      params:delta("LOOP START",d)
-      print("loop start " .. loopStart)
+      params:delta("START POINT",d)
+      loopStart = sPoint
     else
       if pageNum == 1 then
         edit = util.clamp(edit+d,1,#step)
-        print("edit " .. edit)
-      -- elseif pageNum == 2 then
-      --   sel = util.clamp(sel+d,1,#controls)
-      --   print(sel)
       end
     end
   elseif n==3 then
     if KEYDOWN1 == 1 then
-      params:delta("LOOP END",d)
-      print("loop end " .. loopEnd)
+      --params:delta("LOOP END",d)
+      params:delta("END POINT",d)
+      loopEnd = ePoint
     else
       if pageNum == 1 then
         if params:get("clock") == 1 then
@@ -464,10 +467,21 @@ function drawLoop()
   screen.move(2,(1*9)+27)
   screen.line(126,(1*9)+27)
   screen.stroke()
-  screen.level(5)
+  screen.level(4)
   screen.move((loopStart*124)/loopLength-(124/loopLength)+2,(1*9)+27)
   screen.line((loopEnd*124)/loopLength-(124/loopLength)+2,(1*9)+27)
+  
   screen.stroke()
+  -- 
+  screen.level(15)
+  screen.move((sPoint*124)/loopLength-(124/loopLength)+2,(1*9)+26)
+  screen.line_rel(0,1)
+  screen.stroke()
+  screen.level(15)
+  screen.move((ePoint*124)/loopLength-(124/loopLength)+2,(1*9)+26)
+  screen.line_rel(0,1)
+  screen.stroke()
+  --
   screen.level(15)
   screen.move((positions[1]-1)*(124/(loopLength))+2,(1*9)+25)
   screen.line_rel(0,3)
@@ -481,6 +495,15 @@ function drawLoop()
   screen.move((loopStart*124)/loopLength-(124/loopLength)+2,(2*9)+27)
   screen.line((loopEnd*124)/loopLength-(124/loopLength)+2,(2*9)+27)
   screen.stroke()
+  ---
+  screen.level(15)
+  screen.move((sPoint*124)/loopLength-(124/loopLength)+2,(2*9)+26)
+  screen.line_rel(0,1)
+  screen.level(15)
+  screen.move((ePoint*124)/loopLength-(124/loopLength)+2,(2*9)+26)
+  screen.line_rel(0,1)
+  screen.stroke()
+  ---
   screen.level(15)
   screen.move((positions[2]-1)*(124/(loopLength))+2,(2*9)+25)
   screen.line_rel(0,3)
@@ -491,7 +514,7 @@ function drawHelp()
   if pageNum == 2 then
     screen.level(2)
     screen.move(126,10)
-    screen.text_right("1/5")
+    screen.text_right("1/6")
     screen.level(15)
     for i=1,3 do
       screen.move(2,i*10+16)
@@ -512,7 +535,7 @@ function drawHelp()
   elseif pageNum == 3 then
     screen.level(2)
     screen.move(126,10)
-    screen.text_right("2/5")
+    screen.text_right("2/6")
     screen.level(15)
     screen.level(15)
     for i=4,6 do
@@ -527,7 +550,7 @@ function drawHelp()
   elseif pageNum == 4 then
     screen.level(2)
     screen.move(126,10)
-    screen.text_right("3/5")
+    screen.text_right("3/6")
     screen.level(15)
     screen.level(15)
     for i=7,10 do
@@ -542,7 +565,7 @@ function drawHelp()
   elseif pageNum == 5 then
     screen.level(2)
     screen.move(126,10)
-    screen.text_right("4/5")
+    screen.text_right("4/6")
     screen.level(15)
     screen.level(15)
     for i=11,14 do
@@ -557,15 +580,29 @@ function drawHelp()
   elseif pageNum == 6 then
     screen.level(2)
     screen.move(126,10)
-    screen.text_right("5/5")
+    screen.text_right("5/6")
     screen.level(15)
-    for i=15,18 do
+    for i=15,17 do
       screen.move(2,((i-14)*10)+16)
       screen.text(label[i])
     end
     screen.level(4)
-    for i=15,18 do
+    for i=15,17 do
       screen.move(10,((i-14)*10)+16)
+      screen.text(description[i])
+    end
+  elseif pageNum == 7 then
+    screen.level(2)
+    screen.move(126,10)
+    screen.text_right("6/6")
+    screen.level(15)
+    for i=18,19 do
+      screen.move(2,((i-17)*10)+16)
+      screen.text(label[i])
+    end
+    screen.level(4)
+    for i=18,19 do
+      screen.move(10,((i-17)*10)+16)
       screen.text(description[i])
     end
   end
